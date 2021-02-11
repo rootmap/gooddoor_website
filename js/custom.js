c2301e3316e319dd0cf80b6bdade85d0077fc8b0
MD MAHAMUDUR ZAMAN BHUYIAN <32055228+rootmap@users.noreply.github.com>
2021-02-12 02:05:44 +0600

    init news

--- js/custom.js
+++ js/custom.js
@@ -12,3 +12,44 @@ $(document).on("scroll", function(){
         
     }
 });
+
+$(".accordian").on('click',function(){
+    //alert(1);
+    var accordian = $(this).children('i').attr('class');
+    console.log(accordian);
+    if(accordian == "bi bi-caret-down-fill color-tia pr-2")
+    {
+        $(this).children('i').removeClass('bi-caret-down-fill');
+        $(this).children('i').addClass('bi-caret-right-fill');
+
+        $("#"+$(this).attr('data-id')).hide();
+    }
+    else
+    {
+        if(accordian == "bi color-tia pr-2 bi-caret-right-fill")
+        {
+            $(this).children('i').removeClass('bi-caret-right-fill');
+            $(this).children('i').addClass('bi-caret-down-fill');
+            $("#"+$(this).attr('data-id')).show();
+        }
+        else
+        {
+            if(accordian == "bi color-tia pr-2 bi-caret-down-fill")
+            {
+                $(this).children('i').removeClass('bi-caret-down-fill');
+                $(this).children('i').addClass('bi-caret-right-fill');
+                $("#"+$(this).attr('data-id')).hide();
+            }
+            else
+            {
+                $(this).children('i').removeClass('bi-caret-right-fill');
+                $(this).children('i').addClass('bi-caret-down-fill');
+                $("#"+$(this).attr('data-id')).show();
+            }
+        }
+    
+        
+    }
+
+
+});
